506435be4a72b60bd4f8f0542aa0ef5a72373d5d
vikrambenipal <vikrambenipal@gmail.com>
2021-02-16 12:49:21 -0500

    added click event that calls API

--- script.js
+++ script.js
@@ -6,16 +6,22 @@ const error = document.getElementById('error');
 // onSubmit call API and get info to populate fields 
 
 const api_key = "at_A2hg6w4veqrY2FddQRLU4aaREDvfm";
-let ip = "dsdsdssd8.8.8.8";
+let ip = "8.8.8.8";
 
-// Request API 
-$.get({
-    url: "https://geo.ipify.org/api/v1",
-    data: {apiKey: api_key, ipAddress: ip},
-    success: function(data) {
-        $("body").append("<pre>"+ JSON.stringify(data,"",2)+"</pre>");
-    }
-}).fail(function (){
-    // invalid IP address case 
-    console.log("error");
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    // Request API 
+    $.get({
+        url: "https://geo.ipify.org/api/v1",
+        data: {apiKey: api_key, ipAddress: ip},
+        success: function(data) {
+            $("body").append("<pre>"+ JSON.stringify(data,"",2)+"</pre>");
+        }
+    }).fail(function (){
+        // invalid IP address case 
+        console.log("error");
+        error.innerText = "Error: Please enter a valid IP address";
+    })
 })
+
+
